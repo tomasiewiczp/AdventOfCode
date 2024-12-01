26e43e3bc39d068d907f54b7b7029450c481bf1d
Pawel Tomasiewicz <ptomasiewicz.budownictwo@gmail.com>
2024-12-01 22:27:47 +0100

    improvements to AOC 2024 day 1/1

--- AOC2024/day_1/part1.py
+++ AOC2024/day_1/part1.py
@@ -1,7 +1,7 @@
 # task url: https://adventofcode.com/2024/day/1
 
 with open('input.txt', 'r') as file:
-    lists = [list(map(int, line.strip().split())) for line in file]
+    lists = [tuple(map(int, line.strip().split())) for line in file]
     l1 = sorted(line[0] for line in lists)
     l2 = sorted(line[1] for line in lists)
     total_distance = sum(abs(l1[i]-l2[i]) for i in range(len(l1)))
